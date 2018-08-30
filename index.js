0028cd340fd1e5b67c318a3f72d2c5968045a345
github username <github email address>
2018-08-30 10:57:02 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = drivers => drivers.slice(0, 2)
+
+const returnLastTwoDrivers = drivers => drivers.slice(-2)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = num => multiplier => num * multiplier
+
+const fareDoubler = num => num * 2
+
+const fareTripler = num => num * 3
+
+const selectDifferentDrivers = (arrayOfDrivers, func) => func(arrayOfDrivers)
